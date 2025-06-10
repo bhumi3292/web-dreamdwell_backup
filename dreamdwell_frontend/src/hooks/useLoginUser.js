eba0fcb872470291e48af5c22a7ca831468c8eb8
bhumi3292 <137974891+bhumi3292@users.noreply.github.com>
2025-06-10 21:52:16 +0545

    login_hook

---
+++ dreamdwell_frontend/src/hooks/useLoginUser.js
@@ -0,0 +1,21 @@
+import { toast } from "react-toastify";
+import { loginUserService } from "../services/authService";
+import { useMutation } from "@tanstack/react-query";
+import {AuthContext} from "../auth/authProvider";
+import { useContext } from "react";
+export const useLoginUser = () => {
+    const { login } = useContext(AuthContext);
+
+    return useMutation({
+        mutationFn: loginUserService,
+        mutationKey: ['login-key'],
+        onSuccess: (data) => {
+
+            login(data?.user, data?.token);
+            toast.success(data?.message || "Login Success");
+        },
+        onError: (err) => {
+            toast.error(err?.message || "Login Failed");
+        },
+    });
+}
